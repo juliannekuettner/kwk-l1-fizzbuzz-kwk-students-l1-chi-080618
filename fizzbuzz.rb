953d9c54df41be6764f68288794becc38beca548
Julianne Kuettner <julianne.kuettner@thenobleacademy.org>
2018-08-07 20:40:14 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+
+def 
+   if number % 3 == 0 && number % 5 == 0
+   return "FizzBuzz"
+  elsif number % 3 == 0 
+    return "Fizz"
+  elsif number % 5 == 0 
+    return "Buzz"
+  else
+   return "nil"
+ end
+end
